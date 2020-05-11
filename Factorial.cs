e9d13e5a6bcad528fb621e86999037eaacf10de3
Ashwini <ashwini.itc@gmail.com>
2020-05-11 16:06:10 -0700

    fact
    factorial file

---
+++ Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AshwiniApp
+{
+    class Factorial
+    {
+
+
+        public static void printFactorial(int n) {
+            int fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact = fact * i;
+             }
+            Console.WriteLine("The factorial of the number {0} is {1}",n,fact);
+        }
+    }
+}
